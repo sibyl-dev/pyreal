d084ff6903600c5756f8f046311f5fa0f0535850
zyteka <zyteka@mit.edu>
2020-04-22 15:03:00 -0400

    Added example explanations

--- explanation_toolkit/example_explanation.py
+++ explanation_toolkit/example_explanation.py
@@ -1,64 +1,80 @@
-# TODO: Under construction, needs refactoring
 from sklearn.neighbors import NearestNeighbors
-
-nbrs = None
-saved_y = None
+import numpy as np
 
 
-def fit_nearest_neighbor(X, y=None):
-    """
-    Fit the nearest neighbor algorithm to the dataset.
+class NearestNeighborExplanation:
+    def __init__(self):
+        self.nbrs = None
+        self.y = None
 
-    :param X: the dataset to search
-    :param y: the true values that accompany X
-    :return: None
-    """
-    global nbrs
-    global saved_y
-    nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(X)
-    saved_y = y
+    def fit_nearest_neighbor(self, X, y=None):
+        """
+        Fit the nearest neighbor algorithm to the dataset.
 
+        :param X: array_like of shape (n_samples, n_features)
+                  The dataset to search for neighbors in
+        :param y: array_like of shape (n_samples, )
+                  The true values that accompany X
+        :return: None
+        """
+        self.nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(X)
+        self.y = np.asanyarray(y)
 
-def nearest_neighbor(x, N=1, desired_y=None, ys=None,
-                     search_by=10, search_depth=1000):
-    """
-    Find the most similar input to x in the neighbors list.
-    Requires fit_nearest_neighbor to be called first
+    def nearest_neighbor(self, x, N=1, desired_y=None, y=None,
+                         search_by=10, search_depth=1000):
+        """
+        Find the most similar input to x in the neighbors list.
+        Requires fit_nearest_neighbor to be called first
 
-    :param x: the input to search for similar inputs to
-    :param N: The number of neighbors to return
-    :param desired_y: Only return neighbors with this y
-    :param ys: ys associated with the dataset
-    :param search_by: Number of neighbors to look at at a time when searching
-                      for a desired y
-    :param search_depth: The maximum number of neighbors to look at before
-                         returning
-    :return: the index in X (from fit_nearest_neighbor) of the nearest input
-    """
-    assert(nbrs is not None,
-           "Must call fit_nearest_neighbor before calling nearest_neighbor")
+        :param x: array_like of shape (n_features, )
+               The input to search for similar inputs to
+        :param N: integer
+               The number of neighbors to return
+        :param desired_y: object
+               Only return neighbors with this y
+        :param y: array_like of size (n_samples, )
+               True values associated with the dataset, not required if y was
+               provided at fitting or desired_y is None
+        :param search_by: integer
+               Number of neighbors to look at at a time when searching
+               for a desired y
+        :param search_depth: integer
+               The maximum number of neighbors to look at before returning
+        :return: integer
+                 The row index in X (from fit_nearest_neighbor) of the nearest
+                 input
+        :except AssertError
+                If fit_nearest_neighbor has not been called yet
+        :except ValueError
+                If desired_y is not None, but y is None and no y was provided
+                at fitting time
+        """
+        assert(self.nbrs is not None,
+               "Must call fit_nearest_neighbor before calling nearest_neighbor")
 
-    if desired_y is None:
-        nbr_inds = nbrs.kneighbors(x, return_distance=False, n_neighbors=N)[0]
-        return nbr_inds
+        if desired_y is None:
+            nbr_inds = self.nbrs.kneighbors(
+                x, return_distance=False, n_neighbors=N)[0]
+            return nbr_inds
 
-    if desired_y is not None and saved_y is None and ys is None:
-        raise ValueError("Must provide true values when requesting a y if "
-                         "they were not provided when fitting")
-    if desired_y is not None:
-        current_search_N = search_by
-        inds = []
-        while True:
-            nbr_inds = nbrs.kneighbors(x, return_distance=False,
-                                       n_neighbors=current_search_N)[0]
-            for ind in nbr_inds:
-                y = ys.iloc[ind]
-                if y == desired_y:
-                    inds.append(ind)
-                    if len(inds) >= N:
-                        return inds
-            current_search_N += search_by
-            if current_search_N > search_depth:
-                print("Couldn't find enough appropriate " +
-                      "neighbors within search_depth")
-                return inds
+        if desired_y is not None and self.y is None and y is None:
+            raise ValueError("Must provide true values when requesting a y if "
+                             "they were not provided when fitting")
+        if desired_y is not None:
+            y = np.asanyarray(y)
+            current_search_N = search_by
+            inds = []
+            while True:
+                nbr_inds = self.nbrs.kneighbors(x, return_distance=False,
+                                                n_neighbors=current_search_N)[0]
+                for ind in nbr_inds:
+                    this_y = y[ind]
+                    if this_y == desired_y:
+                        inds.append(ind)
+                        if len(inds) >= N:
+                            return inds
+                current_search_N += search_by
+                if current_search_N > search_depth:
+                    print("Couldn't find enough appropriate " +
+                          "neighbors within search_depth")
+                    return inds
---
+++ tests/test_example_explanation.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `explanation_toolkit` package."""
+
+import unittest
+import numpy as np
+import pandas as pd
+
+from explanation_toolkit import example_explanation
+
+
+class TestExampleExplanation(unittest.TestCase):
+    """Tests for `explanation_toolkit` package."""
+
+    def setUp(self):
+        self.ee = example_explanation.NearestNeighborExplanation()
+        self.X = [[0, 0, 0],
+                  [1, 1, 1],
+                  [2, 3, 2],
+                  [10, 11, 12]]
+        self.y = [1, 1, 0, 0]
+
+    def tearDown(self):
+        pass
+
+    def test_fit_nearest_neighbor(self):
+        self.assertIsNone(self.ee.nbrs)
+        self.assertIsNone(self.ee.y)
+
+        output = self.ee.fit_nearest_neighbor(self.X)
+        self.assertIsNotNone(self.ee.nbrs)
+        self.assertIsNone(self.ee.y)
+        self.assertIsNone(output)
+
+        output = self.ee.fit_nearest_neighbor(np.array(self.X), self.y)
+        self.assertIsNotNone(self.ee.nbrs)
+        self.assertIsNotNone(self.ee.y)
+        self.assertIsNone(output)
+
+        self.ee.fit_nearest_neighbor(pd.DataFrame(self.X), np.array(self.y))
+        self.ee.fit_nearest_neighbor(pd.DataFrame(self.X), pd.Series(self.y))
+
+    def test_nearest_neighbor(self):
+        """Test something."""
+        self.assertTrue(True)
