a9fbffb3f25eb02b95e471fe944faf0e197e152a
zyteka <zyteka@mit.edu>
2020-09-24 16:28:49 -0400

    Added base explainer class

---
+++ sibyl/explainers/base.py
@@ -0,0 +1,136 @@
+from abc import ABC, abstractmethod
+from sibyl.utils import model_utils
+
+
+class Explainer(ABC):
+    def __init__(self, model_pickle_filepath,
+                 X_orig, y_orig=None,
+                 feature_descriptions=None,
+                 e_transforms=None, m_transforms=None, i_transforms=None,
+                 fit_on_init=False):
+        """
+        Initialize an Explainer object
+        :param model_pickle_filepath: filepath
+               Filepath to the pickled model to explain
+        :param X_orig: dataframe of shape (n_instances, x_orig_feature_count)
+               The training set for the explainer
+        :param y_orig: dataframe of shape (n_instances,)
+               The y values for the dataset
+        :param feature_descriptions: dict
+               Interpretable descriptions of each feature
+        :param e_transforms: transformer object or list of transformer objects
+               Transformer(s) that need to be used on x_orig for the explanation algorithm:
+                    x_orig -> x_explain
+        :param m_transforms: transformer object or list of transformer objects
+               Transformer(s) needed on x_orig to make predictions on the dataset with model, if different
+               than ex_transforms
+                    x_orig -> x_model
+        :param i_transforms: transformer object or list of transformer objects
+               Transformer(s) needed to make x_orig interpretable
+                    x_orig -> x_interpret
+        :param fit_on_init: Boolean
+               If True, fit the explainer on initiation.
+               If False, self.fit() must be manually called before produce() is called
+        """
+        # TODO: check is model has .predict function
+        # TODO: check if transformer(s) have transform
+        # TODO: add multiple different types of model reading utilities, and select one
+
+        self.model = model_utils.load_model_from_pickle(model_pickle_filepath)
+
+        self.X_orig = X_orig
+        self.y_orig = y_orig
+
+        self.expected_feature_number = X_orig.shape[1]
+
+        self.x_orig_feature_count = X_orig.shape[1]
+
+        self.i_transforms = i_transforms
+        self.m_transforms = m_transforms
+        self.e_transforms = e_transforms
+        if not isinstance(e_transforms, list):
+            self.e_transformers = [e_transforms]
+        else:
+            self.e_transformers = e_transforms
+        if not isinstance(m_transforms, list):
+            self.e_transformers = [m_transforms]
+        else:
+            self.e_transformers = m_transforms
+        if not isinstance(i_transforms, list):
+            self.e_transformers = [i_transforms]
+        else:
+            self.e_transformers = i_transforms
+
+        self.feature_descriptions = feature_descriptions
+
+        if fit_on_init:
+            self.fit()
+
+    @abstractmethod
+    def fit(self):
+        """
+        Fit this explainer object
+        :return: None
+        """
+        pass
+
+    @abstractmethod
+    def produce(self, x):
+        """
+        Return the explanation
+        :return:
+        """
+        pass
+
+    def transform_to_x_explain(self, x_orig):
+        """
+        Transform x_orig to x_explain, using the e_transforms
+        :param x_orig: DataFrame of shape (n_instances, x_orig_feature_count)
+        :return: x_explain: DataFrame of shape (n_instances, x_explain_feature_count)
+        """
+        x_explain = x_orig.copy()
+        for transform in self.e_transforms:
+            x_explain = transform.transform(x_explain)
+        return x_explain
+
+    def transform_to_x_model(self, x_orig):
+        """
+        Transform x_orig to x_model, using the e_transforms
+        :param x_orig: DataFrame of shape (n_instances, x_orig_feature_count)
+        :return: x_model: DataFrame of shape (n_instances, x_model_feature_count)
+        """
+        x_model = x_orig.copy()
+        for transform in self.m_transforms:
+            x_model = transform.transform(x_model)
+        return x_model
+
+    def transform_to_x_interpret(self, x_orig):
+        """
+        Transform x_orig to x_interpret, using the e_transforms
+        :param x_orig: DataFrame of shape (n_instances, x_orig_feature_count)
+        :return: x_interpret: DataFrame of shape (n_instances, x_interpret_feature_count)
+        """
+        x_interpret = x_orig.copy()
+        for transform in self.i_transforms:
+            x_interpret = transform.transform(x_interpret)
+        return x_interpret
+
+    def model_predict(self, x_orig):
+        """
+        Predict on x_orig using the model and return the result
+        :param x_orig: DataFrame of shape (n_instances, x_orig_feature_count)
+        :return: prediction
+        """
+        x_model = self.transform_to_x_model(x_orig)
+        return self.model.predict(x_model)
+
+    def feature_description(self, feature_name):
+        """
+        Returns the interpretable description associated with a feature
+        :param feature_name: string
+        :return: string
+                 Description of feature
+        """
+        return self.feature_descriptions[feature_name]
+
+
--- sibyl/explainers/local_feature_explanation.py
+++ sibyl/explainers/local_feature_explanation.py
@@ -1,67 +1,64 @@
 from shap import KernelExplainer, LinearExplainer
+from sibyl.explainers.base import Explainer
 import numpy as np
 import pickle
+from abc import ABC, abstractmethod
 
-# CLASS WRAPPERS ----------------
 
-
-class FeatureContributionExplainer:
-    def __init__(self, model, dataset, transformers=None,
-                 algorithm="shap", fit_on_init=True):
+class FeatureContributionExplainer(Explainer):
+    def __init__(self, *args, e_algorithm="shap"):
         """
         Initial a FeatureContributions object
         :param model: model object
                The model to explain
-        :param dataset: dataframe of shape (n_instances, n_features)
+        :param x_orig: dataframe of shape (n_instances, n_features)
                The training set for the explainer
-        :param transformers: transformer object or list of transformer objects
-               Transformer(s) to use before getting contributions
-        :param algorithm: one of ["shap"]
+        :param y_orig: dataframe of shape (n_instances,)
+               The y values for the dataset
+        :param e_transforms: transformer object or list of transformer objects
+               Transformer(s) needed to run explanation algorithm on dataset
+        :param m_transforms: transformer object of list of transformer objects
+               Transformer(s) needed to make predictions on the dataset with model, if different
+               than e_transforms
+        :param e_algorithm: one of ["shap"]
         :param fit_on_init: Boolean
                If True, fit the feature contribution explainer on initiation.
                If False, explainer will be set to None and must be fit before
                          get_contributions is called
         """
-        self.base_explainer = None
-        # TODO: check is model has .predict function
-        # TODO: check if transformer(s) have transform
-        # TODO: add some functionality to automatically pick algorithm
-        if not isinstance(transformers, list):
-            self.transformers = [transformers]
-        else:
-            self.transformers = transformers
-        self.expected_feature_number = dataset.shape[1]
+        super(FeatureContributionExplainer, self).__init__(*args)
 
-        if algorithm is None:
-            algorithm = choose_algorithm(model)
-        if algorithm not in ["shap"]:
-            raise ValueError("Algorithm %s not understood" % algorithm)
+        # TODO: add some functionality to automatically pick e_algorithm
+        if e_algorithm is None:
+            e_algorithm = choose_algorithm(self.model)
+        if e_algorithm not in ["shap"]:
+            raise ValueError("Algorithm %s not understood" % e_algorithm)
 
-        self.algorithm = algorithm
-        self.model = model
-        self.dataset = dataset
-
-        if self.transformers is not None:
-            for transformer in self.transformers:
-                dataset = transformer.transform(dataset)
-
+        self.algorithm = e_algorithm
         self.explainer = None
 
-        if fit_on_init:
-            self.fit_contributions()
-
-    def fit_contributions(self):
+    def fit(self):
+        """
+        Fit the contribution explainer
+        """
+        # TODO: if model is linear sklearn, set explainer type to linear
+        explainer_type = "kernel"
         if self.algorithm == "shap":
-            # TODO: if model is linear sklearn, set explainer type to linear
-            self.explainer = fit_contributions_shap(self.model, self.dataset,
-                                                    savefile=None, return_result=True,
-                                                    explainer_type="linear")
+            dataset = self.transform_to_x_explain(np.asanyarray(self.X_orig))
+            if explainer_type is "kernel":
+                self.explainer = KernelExplainer(self.model, dataset)
+            elif explainer_type is "linear":
+                self.explainer = LinearExplainer(self.model, dataset)
+            else:
+                raise ValueError("Unrecognized shap type %s" % type)
 
-    def get_contributions(self, x):
+    def produce(self, x):
         """
         Returns the contributions of each feature in x
-        :param x:
-        :return:
+        :param x: DataFrame of shape (n_instances, n_features)
+               The input to be explained
+        :return: DataFrame of shape (n_instances, n_features
+                 The contribution of each feature
         """
         if self.explainer is None:
             raise AttributeError("Instance has no explainer. Must call "
@@ -72,10 +69,10 @@ class FeatureContributionExplainer:
                              "Expected ({},), received {}"
                              .format(self.expected_feature_number, x.shape))
         if self.algorithm == "shap":
-            return get_contributions_shap(x, self.explainer)
+            x = np.asanyarray(x)
+            contributions = self.explainer.shap_values(x)
+            return contributions
 
-
-# STATELESS IMPLEMENTATIONS --------------
 
 def choose_algorithm(model):
     """
@@ -87,50 +84,5 @@ def choose_algorithm(model):
     return "shap"
 
 
-def fit_contributions_shap(model, dataset,
-                           savefile=None, return_result=False,
-                           explainer_type="kernel"):
-    """
-    Fit a contribution explainer for the shap algorithm.
-
-    :param model: function array-like (dataset.shape) -> (dataset.shape[0])
-          The prediction function of the model to explain
-    :param dataset: array-like of shape (n_instances, n_features)
-          The training set for the model
-    :param savefile: file object
-          Where the save the explainer. If None, don't save
-    :param return_result: boolean
-          If true, return the resulting explainer, else return none
-    :param explainer_type: one of ["kernel", "linear"]
-           The type of shap explainer to fit
-    :return: explainer or None
-             Returns the explainer if return_result is True
-    """
-    dataset = np.asanyarray(dataset)
-    if explainer_type is "kernel":
-        explainer = KernelExplainer(model, dataset)
-    elif explainer_type is "linear":
-        explainer = LinearExplainer(model, dataset)
-    else:
-        raise ValueError("Unrecognized shap type %s" % type)
-    if savefile is not None:
-        pickle.dump(explainer, savefile)
-    if return_result:
-        return explainer
-
-
-def get_contributions_shap(x, explainer):
-    """
-    Get the feature contributions for all features in x.
-
-    :param x: array_like of shape (n_features,)
-           The input into the model
-    :param explainer: pretrained SHAP explainer
-    :return: array_like of shape (n_features,)
-             The contributions of each feature in x
-    """
-    x = np.asanyarray(x)
-    contributions = explainer.shap_values(x)
-    return contributions
 
 
