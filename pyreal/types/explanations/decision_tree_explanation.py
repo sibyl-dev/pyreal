d4a9818407bd76c5fa6b71a00856af3b0768d96c
zyteka <zyteka@mit.edu>
2021-10-22 10:47:46 -0400

    Completed unit tests

--- pyreal/types/explanations/decision_tree_explanation.py
+++ pyreal/types/explanations/decision_tree_explanation.py
@@ -1,10 +1,16 @@
-from pyreal.types.explanations.base_explanation import ExplanationType
-
 from sklearn.tree import BaseDecisionTree
+from sklearn.utils.validation import check_is_fitted
+from sklearn.exceptions import NotFittedError
+
+from pyreal.types.explanations.base_explanation import ExplanationType
 
 
 class DecisionTreeExplanationType(ExplanationType):
-    @staticmethod
-    def validate(explanation):
-        if not isinstance(explanation, BaseDecisionTree):
+    def validate(self):
+        if not isinstance(self.explanation, BaseDecisionTree):
             raise AssertionError("Decision tree explanations must be sklearn Decision Trees")
+        try:
+            check_is_fitted(self.explanation)
+        except NotFittedError:
+            raise AssertionError(
+                "Decision tree explanations must be fitted sklearn Decision Trees")
---
+++ tests/types/explanations/test_decision_tree_explanation.py
@@ -0,0 +1,26 @@
+from pyreal.types.explanations.decision_tree_explanation import DecisionTreeExplanationType
+from sklearn.tree import DecisionTreeClassifier
+import pytest
+
+
+def test_decision_tree_explanation():
+    valid_explanation = DecisionTreeClassifier().fit([[1, 1, 1], [2, 2, 2]], [1, 2])
+    explanation = DecisionTreeExplanationType(valid_explanation)
+    explanation.validate()  # assert does not raise
+    assert explanation.get() is valid_explanation
+
+    non_tree_explanation = [1, 1, 1]
+    with pytest.raises(AssertionError):
+        DecisionTreeExplanationType(non_tree_explanation)
+    # skip over the init validation to check validate separately
+    explanation.explanation = non_tree_explanation
+    with pytest.raises(AssertionError):
+        explanation.validate()
+
+    unfit_explanation = DecisionTreeClassifier()
+    with pytest.raises(AssertionError):
+        DecisionTreeExplanationType(unfit_explanation)
+    # skip over the init validation to check validate separately
+    explanation.explanation = non_tree_explanation
+    with pytest.raises(AssertionError):
+        explanation.validate()
