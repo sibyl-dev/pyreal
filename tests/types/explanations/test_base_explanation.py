4dcbda7cf177bd0818d12dc108c9c32b73ed3a40
zyteka <zyteka@mit.edu>
2021-10-22 10:15:24 -0400

    Adding unit tests

--- tests/types/explanations/test_base_explanation.py
+++ tests/types/explanations/test_base_explanation.py
@@ -10,4 +10,4 @@ def test_explanation_type():
     base_explanation = BaseExplanation(5)
     explanation = ExplanationType(base_explanation)
 
-    assert explanation.get() == base_explanation
+    assert explanation.get() is base_explanation
--- tests/types/explanations/test_dataframe_explanation.py
+++ tests/types/explanations/test_dataframe_explanation.py
@@ -1,12 +1,38 @@
 import pandas as pd
+import pytest
 
 from pyreal.types.explanations.dataframe_explanation import DataFrameExplanationType
 
 
 def test_dataframe_explanation_type():
     valid_explanation = pd.DataFrame([[1, 1, 1]], columns=["A", "B", "C"])
-    invalid_explanation = [[1, 1, 1]]
     explanation = DataFrameExplanationType(valid_explanation)
-    assert explanation.validate(valid_explanation)
+    explanation.validate()  # assert does not raise
+    assert explanation.get() is valid_explanation
 
-    assert explanation.get() == base_explanation
+    invalid_explanation = [[1, 1, 1]]
+    with pytest.raises(AssertionError):
+        DataFrameExplanationType(invalid_explanation)
+    # skip over the init validation to check validate separately
+    explanation.explanation = invalid_explanation
+    with pytest.raises(AssertionError):
+        explanation.validate()
+
+
+def test_feature_importance_explanation_type():
+    valid_explanation = pd.DataFrame([[1, 1, 1]], columns=["A", "B", "C"])
+    explanation = DataFrameExplanationType(valid_explanation)
+    explanation.validate()  # assert does not raise
+    assert explanation.get() is valid_explanation
+
+    invalid_explanation = pd.DataFrame([[1, 1, 1], [2, 2, 2]], columns=["A", "B", "C"])
+    with pytest.raises(AssertionError):
+        DataFrameExplanationType(invalid_explanation)
+    # skip over the init validation to check validate separately
+    explanation.explanation = invalid_explanation
+    with pytest.raises(AssertionError):
+        explanation.validate()
+
+
+
+
