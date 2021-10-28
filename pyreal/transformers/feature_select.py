bbb6073bc4ee87aadfa44bc671bd7f6f11c74709
zyteka <zyteka@mit.edu>
2021-10-28 11:33:21 -0400

    Renaming x_orig to x, transformer inputs to columns, base_transformer to wrapped_transformer

--- pyreal/transformers/feature_select.py
+++ pyreal/transformers/feature_select.py
@@ -2,11 +2,11 @@ from pyreal.transformers import BaseTransformer
 
 
 class FeatureSelectTransformer(BaseTransformer):
-    def __init__(self, feature_names):
-        self.feature_names = feature_names
+    def __init__(self, columns):
+        self.columns = columns
 
     def transform(self, data):
-        return data[self.feature_names]
+        return data[self.columns]
 
 
 class ColumnDropTransformer(BaseTransformer):
@@ -14,18 +14,18 @@ class ColumnDropTransformer(BaseTransformer):
     Removes columns that should not be predictive
     """
 
-    def __init__(self, columns_to_drop):
-        self.columns_to_drop = columns_to_drop
+    def __init__(self, columns):
+        self.columns = columns
 
     def transform(self, x):
-        return x.drop(self.columns_to_drop, axis="columns")
+        return x.drop(self.columns, axis="columns")
 
     def transform_explanation_shap(self, explanation):
-        for col in self.columns_to_drop:
+        for col in self.columns:
             explanation[col] = 0
         return explanation
 
     def transform_explanation_permutation_importance(self, explanation):
-        for col in self.columns_to_drop:
+        for col in self.columns:
             explanation[col] = 0
         return explanation
