beadd5c581046de520684cb8fed0b01f03e93211
zyteka <zyteka@mit.edu>
2021-11-02 14:08:57 -0400

    Removing unneeded evaluate_variation functions

--- pyreal/benchmark/challenges/gfi/shap_feature_importance_challenge.py
+++ pyreal/benchmark/challenges/gfi/shap_feature_importance_challenge.py
@@ -1,4 +1,3 @@
-import numpy as np
 
 from pyreal.benchmark.challenges.explainer_challenge import ExplainerChallenge
 from pyreal.explainers import ShapFeatureImportance
@@ -7,8 +6,4 @@ from pyreal.explainers import ShapFeatureImportance
 class ShapFeatureImportanceChallenge(ExplainerChallenge):
     def create_explainer(self):
         return ShapFeatureImportance(model=self.dataset.model, x_train_orig=self.dataset.X,
-                                     transforms=self.dataset.transforms, fit_on_init=True)
-
-    def evaluate_variation(self, results):
-        # TODO: consider alternative evaluation approaches
-        return np.max(np.var(results, axis=0))
+                                     transformers=self.dataset.transforms, fit_on_init=True)
--- pyreal/benchmark/challenges/lfc/local_feature_contribution_challenge.py
+++ pyreal/benchmark/challenges/lfc/local_feature_contribution_challenge.py
@@ -1,4 +1,3 @@
-import numpy as np
 
 from pyreal.benchmark.challenges.explainer_challenge import ExplainerChallenge
 from pyreal.explainers import LocalFeatureContribution
@@ -7,8 +6,4 @@ from pyreal.explainers import LocalFeatureContribution
 class LocalFeatureContributionChallenge(ExplainerChallenge):
     def create_explainer(self):
         return LocalFeatureContribution(model=self.dataset.model, x_train_orig=self.dataset.X,
-                                        transforms=self.dataset.transforms, fit_on_init=True)
-
-    def evaluate_variation(self, results):
-        # TODO: consider alternative evaluation approaches
-        return np.max(np.var(results, axis=0))
+                                        transformers=self.dataset.transforms, fit_on_init=True)
