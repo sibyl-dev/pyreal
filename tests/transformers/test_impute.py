54e80a390914b6e49e48289df92ee3074c5be160
zyteka <zyteka@mit.edu>
2022-02-11 11:31:22 -0500

    Adding unit tests

--- tests/transformers/test_impute.py
+++ tests/transformers/test_impute.py
@@ -24,3 +24,41 @@ def test_fit_transform_multitype_imputer():
 
     row_result = imputer.transform(row)
     assert_frame_equal(expected_row, row_result, check_dtype=False)
+
+
+def test_fit_transform_multitype_imputer_cat_only():
+    imputer = MultiTypeImputer()
+    x = pd.DataFrame([['a', '+'],
+                      ['a', '-'],
+                      [np.nan, '-']], columns=["D", "E"])
+
+    expected_result = pd.DataFrame([['a', '+'],
+                                    ['a', '-'],
+                                    ['a', '-']], columns=["D", "E"])
+    result = imputer.fit_transform(x)
+    assert_frame_equal(expected_result, result, check_dtype=False)
+
+    row = pd.Series([np.nan, '+'], index=["D", "E"])
+    expected_row = pd.DataFrame([['a', '+']], columns=["D", "E"])
+
+    row_result = imputer.transform(row)
+    assert_frame_equal(expected_row, row_result, check_dtype=False)
+
+
+def test_fit_transform_multitype_imputer_num_only():
+    imputer = MultiTypeImputer()
+    x = pd.DataFrame([[3, 1],
+                      [1, 1],
+                      [np.nan, 1]], columns=["A", "B"])
+
+    expected_result = pd.DataFrame([[3, 1],
+                                    [1, 1],
+                                    [2, 1]], columns=["A", "B"])
+    result = imputer.fit_transform(x)
+    assert_frame_equal(expected_result, result, check_dtype=False)
+
+    row = pd.Series([np.nan, 2], index=["A", "B"])
+    expected_row = pd.DataFrame([[2, 2]], columns=["A", "B"])
+
+    row_result = imputer.transform(row)
+    assert_frame_equal(expected_row, row_result, check_dtype=False)
