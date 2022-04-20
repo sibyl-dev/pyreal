4812c274f7692db2fb8b60b418ebde795af99d72
zyteka <zyteka@mit.edu>
2022-04-20 16:22:01 -0400

    functional occlusion

--- pyreal/explainers/time/occlusion_importance.py
+++ pyreal/explainers/time/occlusion_importance.py
@@ -1,21 +1,17 @@
 import numpy as np
-import pandas as pd
-from shap import KernelExplainer, LinearExplainer
 
 from pyreal.explainers import TimeSeriesImportanceBase
-from pyreal.types.explanations.feature_based import AdditiveFeatureContributionExplanation
 
 
 class OcclusionImportance(TimeSeriesImportanceBase):
     """
-    IntervalImportance object.
+    OcclusionImportance object.
 
-    A IntervalImportance object creates features of time-series data by aggregating timestamps into
-    intervals and produce explanation using the SHAP algorithm.
+    An OcclusionImportance object judges the relative importance or saliency of each timestep
+    value by iteratively occluding windows of data, and calculating the resulting change in model
+    prediction.
 
-    IntervalImportance explainers expect data in the **model-ready feature space**
-
-    Currently, only classification models explanation is supported.
+    Currently, only classification model explanation is supported.
 
     Args:
         model (string filepath or model object):
@@ -41,7 +37,7 @@ class OcclusionImportance(TimeSeriesImportanceBase):
         """
         return self
 
-    def get_contributions(self, x_orig, width=5, k="avg"):
+    def get_contributions_slow(self, x_orig, width=5, k="avg"):
         """
         Calculate the explanation of each feature in x using SHAP.
 
@@ -53,9 +49,8 @@ class OcclusionImportance(TimeSeriesImportanceBase):
                  The contribution of each feature
         """
         data_length = x_orig.shape[1]
-        print(data_length)
         v = np.zeros((x_orig.shape[1], len(self.classes)))
-        sig = x_orig
+        sig = np.copy(x_orig)
 
         for i in range(1, width):
             occ_test_signal = np.copy(sig)
@@ -64,7 +59,6 @@ class OcclusionImportance(TimeSeriesImportanceBase):
             else:
                 occ_test_signal[:, 0:i] = k
             pred = self.model.predict_proba(occ_test_signal).reshape(-1)
-            #for j in range(0, i):
             for j in range(i, data_length):
                 v[j] += pred
 
@@ -75,7 +69,6 @@ class OcclusionImportance(TimeSeriesImportanceBase):
             else:
                 occ_test_signal[:, i:i + width] = k
             pred = self.model.predict_proba(occ_test_signal).reshape(-1)
-            #for j in range(i, i + width):
             for j in range(0, i):
                 v[j] += pred
             for j in range(i+width, data_length):
@@ -89,21 +82,79 @@ class OcclusionImportance(TimeSeriesImportanceBase):
             else:
                 occ_test_signal[:, data_length - i:data_length] = k
             pred = self.model.predict_proba(occ_test_signal).reshape(-1)
-            #for j in range(data_length - i, data_length):
             for j in range(0, data_length - i):
                 v[j] += pred
 
         print(v)
 
-        predicted_class = self.model.predict(sig)[0][0]
+        predicted_class = self.model.predict(np.copy(sig))[0][0]
         class_index = self.classes.index(predicted_class)
         importance = v[:, class_index]
-        print(importance)
-        #max = width
-        #min = 0
         max = np.amax(importance)
         min = np.amin(importance)
         importance = (importance - min) / (max - min)
-        #importance = 1 - importance
+
+        return importance.reshape(-1)
+
+    def get_contributions(self, x_orig, width=5, k="avg"):
+        """
+        Calculate the explanation of each feature in x using SHAP.
+
+        Args:
+            x_orig (DataFrame of shape (n_instances, n_features)):
+               The input to be explained
+        Returns:
+            DataFrame of shape (n_instances, n_features):
+                 The contribution of each feature
+        """
+        data_length = x_orig.shape[1]
+        v = np.zeros((x_orig.shape[1], len(self.classes)))
+        sig = np.copy(x_orig)
+
+        for i in range(1, width):
+            #occ_test_signal = np.copy(sig)
+            original_signal = sig[:, 0:i]
+            if k == "avg":
+                sig[:, 0:i] = np.average([sig[:, 0], sig[:, i-1]])
+            else:
+                sig[:, 0:i] = k
+            pred = self.model.predict_proba(sig).reshape(-1)
+            for j in range(i, data_length):
+                v[j] += pred
+            sig[:, 0:i] = original_signal
+
+        for i in range(data_length - width):
+            #occ_test_signal = np.copy(sig)
+            original_signal = sig[:, 0:i]
+            if (k == "avg"):
+                sig[:, i:i + width] = np.average([sig[:, i], sig[:, i+width-1]])
+            else:
+                sig[:, i:i + width] = k
+            pred = self.model.predict_proba(sig).reshape(-1)
+            for j in range(0, i):
+                v[j] += pred
+            for j in range(i+width, data_length):
+                v[j] += pred
+            sig[:, 0:i] = original_signal
+
+        for i in range(1, width):
+            #occ_test_signal = np.copy(sig)
+            original_signal = sig[:, 0:i]
+            if (k == "avg"):
+                sig[:, data_length - i:data_length] = \
+                    np.average([sig[:, data_length-i], sig[:, data_length-1]])
+            else:
+                sig[:, data_length - i:data_length] = k
+            pred = self.model.predict_proba(sig).reshape(-1)
+            for j in range(0, data_length - i):
+                v[j] += pred
+            sig[:, 0:i] = original_signal
+
+        predicted_class = self.model.predict(sig)[0][0]
+        class_index = self.classes.index(predicted_class)
+        importance = v[:, class_index]
+        max = np.amax(importance)
+        min = np.amin(importance)
+        importance = (importance - min) / (max - min)
 
         return importance.reshape(-1)
