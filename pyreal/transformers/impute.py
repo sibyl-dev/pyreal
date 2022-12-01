54314328e1f8bba805b76eb98d0bb5748dfc9cc8
zyteka <zyteka@mit.edu>
2022-12-01 15:01:05 -0500

    Removing unused code

--- pyreal/transformers/impute.py
+++ pyreal/transformers/impute.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from sklearn.impute import SimpleImputer
 
 from pyreal.transformers import Transformer
 
@@ -21,8 +20,6 @@ class MultiTypeImputer(Transformer):
 
         self.numeric_cols = None
         self.categorical_cols = None
-        self.numeric_imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
-        self.categorical_imputer = SimpleImputer(missing_values=np.nan, strategy="most_frequent")
         self.means = None
         self.modes = None
         super().__init__(**kwargs)
@@ -53,13 +50,6 @@ class MultiTypeImputer(Transformer):
         if self.modes.shape[0] > 0:
             self.modes = self.modes.iloc[0, :]
 
-        '''if len(self.numeric_cols) == 0 and len(self.categorical_cols) == 0:
-            raise ValueError("No valid numeric or categorical cols")
-        if len(self.numeric_cols) > 0:
-            self.numeric_imputer.fit(x[self.numeric_cols])
-        if len(self.categorical_cols) > 0:
-            self.categorical_imputer.fit(x[self.categorical_cols])'''
-
         super().fit(x)
 
     def data_transform(self, x):
@@ -83,29 +73,6 @@ class MultiTypeImputer(Transformer):
             series_flag = True
             name = x.name
             x = x.to_frame().T
-        '''x[self.columns] = x[self.columns].fillna(value=np.nan)
-        if len(self.categorical_cols) == 0:
-            new_numeric_cols = self.numeric_imputer.transform(x[self.numeric_cols])
-            result = pd.DataFrame(new_numeric_cols, columns=self.numeric_cols, index=x.index)
-
-        elif len(self.numeric_cols) == 0:
-            new_categorical_cols = self.categorical_imputer.transform(x[self.categorical_cols])
-            result = pd.DataFrame(
-                new_categorical_cols, columns=self.categorical_cols, index=x.index
-            )
-
-        else:
-            new_numeric_cols = self.numeric_imputer.transform(x[self.numeric_cols])
-            new_categorical_cols = self.categorical_imputer.transform(x[self.categorical_cols])
-            result = pd.concat(
-                [
-                    pd.DataFrame(new_numeric_cols, columns=self.numeric_cols, index=x.index),
-                    pd.DataFrame(
-                        new_categorical_cols, columns=self.categorical_cols, index=x.index
-                    ),
-                ],
-                axis=1,
-            )'''
 
         result = x.copy()
         result[self.numeric_cols] = result[self.numeric_cols].fillna(value=self.means)
