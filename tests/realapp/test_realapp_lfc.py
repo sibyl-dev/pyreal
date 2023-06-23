7cb135bd4bebc2b6ffb6b37213375be11532faf8
zyteka <zyteka@mit.edu>
2023-06-23 14:41:46 -0400

    Adding test for prepare with id column

--- tests/realapp/test_realapp_lfc.py
+++ tests/realapp/test_realapp_lfc.py
@@ -58,16 +58,19 @@ def test_produce_local_feature_contributions(regression_no_transforms):
 
 
 def test_produce_local_feature_contributions_with_id_column(regression_one_hot):
-    realApp = RealApp(
+    real_app = RealApp(
         regression_one_hot["model"],
         regression_one_hot["x"],
         transformers=regression_one_hot["transformers"],
         id_column="ID",
     )
-    features = ["A", "B", "C"]
 
+    features = ["A", "B", "C"]
     x_multi_dim = pd.DataFrame([[4, 1, 1, "a"], [6, 2, 3, "b"]], columns=features + ["ID"])
-    explanation = realApp.produce_feature_contributions(x_multi_dim)
+
+    real_app.prepare_feature_contributions(x_multi_dim)
+
+    explanation = real_app.produce_feature_contributions(x_multi_dim)
 
     explanation_a = explanation["a"].sort_values(by="Feature Name", axis=0)
     explanation_b = explanation["b"].sort_values(by="Feature Name", axis=0)
