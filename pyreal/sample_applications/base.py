4d90baf437cf0baad923c9eb4e0974451e82dcaa
zyteka <zyteka@mit.edu>
2023-07-24 11:43:42 -0400

    Lint fix

--- pyreal/sample_applications/base.py
+++ pyreal/sample_applications/base.py
@@ -6,7 +6,7 @@ def clean(verbose=False):
     Clean all saved pickle files. Useful after updating dependencies.
     """
     root = os.path.dirname(__file__)
-    num = 0 
+    num = 0
     for root, _, files in os.walk(root):
         for file in files:
             if file.lower().endswith(".pkl"):
