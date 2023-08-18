7a697841371d5c849bcb2181d5740dd4618b4e56
zyteka <zyteka@mit.edu>
2023-08-18 15:49:13 -0400

    Adding appropriate upper and lower bounds

--- pyreal/explainers/example/counterfactual_examples.py
+++ pyreal/explainers/example/counterfactual_examples.py
@@ -12,12 +12,12 @@ def _dist(a, b):
 
 
 class CFProblem(ElementwiseProblem):
-    def __init__(self, length, model, x_algo, target_prediction):
+    def __init__(self, length, model, x_algo, target_prediction, xl, xu):
         self.model = model
         self.target_prediction = target_prediction
         self.x_algo = x_algo.squeeze().values
         super().__init__(n_var=length,
-                         n_obj=4, xl=np.full(length, -10000), xu=np.full(length, 10000))
+                         n_obj=4, xl=xl.squeeze().values, xu=xu.squeeze().values)
 
     def _evaluate(self, x, out, *args, **kwargs):
         # Difference between the prediction on x_mod and the target prediction:
@@ -45,7 +45,6 @@ class Counterfactuals(ExampleBasedBase):
     """
 
     def __init__(self, model, x_train_orig=None, **kwargs):
-        self.explainer = None
         self.cf = None
         super(Counterfactuals, self).__init__(model, x_train_orig, **kwargs)
 
@@ -59,6 +58,9 @@ class Counterfactuals(ExampleBasedBase):
             y_train:
                 Targets of training set, required if not provided on initialization
         """
+        x_train_orig = self._get_x_train_orig(x_train_orig)
+        self.xl = self.transform_to_x_model(x_train_orig).min(axis=0)
+        self.xu = self.transform_to_x_model(x_train_orig).max(axis=0)
         return self
 
     def get_explanation(self, x_orig, target_prediction, num_examples=3):
@@ -75,12 +77,15 @@ class Counterfactuals(ExampleBasedBase):
         Returns:
             CounterfactualExplanation
         """
+        if self.xl is None:
+            raise AttributeError("Must call fit() before produce()")
         x_algo = self.transform_to_x_model(x_orig)
-        problem = CFProblem(x_algo.shape[-1], self.model, x_algo, target_prediction)
-        algorithm = NSGA2(pop_size=100)
+        problem = CFProblem(x_algo.shape[-1], self.model, x_algo, target_prediction, xl=self.xl, xu=self.xu)
+        algorithm = NSGA2(pop_size=1000)
         result = minimize(problem, algorithm, ('n_gen', 200), seed=1)
         print(result.X[0])
         print(result.F[0])
+        print(self.model.predict(result.X[0]))
 
 
 from pyreal.sample_applications import ames_housing
@@ -89,7 +94,7 @@ x_train_orig = ames_housing.load_data(n_rows=100).drop(columns="Id")
 model = ames_housing.load_model()
 transformers = ames_housing.load_transformers()
 
-explainer = Counterfactuals(model, x_train_orig, transformers=transformers)
+explainer = Counterfactuals(model, x_train_orig, transformers=transformers, fit_on_init=True)
 explainer.get_explanation(x_train_orig.iloc[0], target_prediction=200000)
 
 
