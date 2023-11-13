69bb9157d8bed9ac1de706cf645408a4f3f3f6a2
Ola Zytek <zyteka@mit.edu>
2023-11-13 17:15:10 -0500

    Adding unit tests

--- pyreal/benchmark/profiler.py
+++ pyreal/benchmark/profiler.py
@@ -5,12 +5,13 @@ from pyreal.sample_applications import ames_housing
 # Setup
 app = ames_housing.load_app()
 x = ames_housing.load_data()
+x = x.sample(10000, replace=True)
 
 profiler = cProfile.Profile()
 profiler.enable()
 
 # Analysis code
-app.produce_feature_contributions(x)
+app.produce_feature_importance(format_output=False)
 profiler.disable()
 
 stats = pstats.Stats(profiler)
