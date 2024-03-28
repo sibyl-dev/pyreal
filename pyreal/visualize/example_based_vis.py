fcaa08e06c1265a85bd7e19e29150b443ee1d58d
Ola Zytek <zyteka@mit.edu>
2024-03-28 10:31:14 -0700

    Adding show_only_different option to example table

--- pyreal/visualize/example_based_vis.py
+++ pyreal/visualize/example_based_vis.py
@@ -2,7 +2,11 @@ import pandas as pd
 
 
 def example_table(
-    explanation, input_name="Original Input", y_column_name="Ground Truth", y_format_func=None
+    explanation,
+    input_name="Original Input",
+    y_column_name="Ground Truth",
+    y_format_func=None,
+    show_only_different=False,
 ):
     table = explanation["X"].copy()
     y = explanation["y"].copy()
@@ -15,4 +19,6 @@ def example_table(
         y = y.apply(y_format_func)
     y[input_name] = "N/A"
     table.insert(0, y_column_name, y)
+    if show_only_different:
+        table = table.loc[:, table.nunique() > 1]
     return table
